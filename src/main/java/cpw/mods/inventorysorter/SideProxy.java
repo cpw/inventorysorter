14e0510aac623514fb171fbda8e8871386e64ff4
cpw <cpw@weeksfamily.ca>
2016-01-16 22:02:29 -0500

    Fix copyright boilerplate. Intellij copyright plugin ftw!

--- src/main/java/cpw/mods/inventorysorter/SideProxy.java
+++ src/main/java/cpw/mods/inventorysorter/SideProxy.java
@@ -1,19 +1,19 @@
 /*
- *     Copyright
- *     This file is part of inventorysorter.
+ *     Copyright © 2016 cpw
+ *     This file is part of Inventorysorter.
  *
- *     Foobar is free software: you can redistribute it and/or modify
+ *     Inventorysorter is free software: you can redistribute it and/or modify
  *     it under the terms of the GNU General Public License as published by
  *     the Free Software Foundation, either version 3 of the License, or
  *     (at your option) any later version.
  *
- *     Foobar is distributed in the hope that it will be useful,
+ *     Inventorysorter is distributed in the hope that it will be useful,
  *     but WITHOUT ANY WARRANTY; without even the implied warranty of
  *     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  *     GNU General Public License for more details.
  *
  *     You should have received a copy of the GNU General Public License
- *     along with inventorysorter.  If not, see <http://www.gnu.org/licenses/>.
+ *     along with Inventorysorter.  If not, see <http://www.gnu.org/licenses/>.
  */
 
 package cpw.mods.inventorysorter;
